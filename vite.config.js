749d439e3b08859141b1948618279e2d8bf5b147
rajZadafiya55 <zadafiyaraj395>
2024-07-14 10:39:25 +0530

    modified subscription

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
     port: 4000,
-    host: true
-  }
-})
+    host: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+});
